7b624600c4818874daa9771015d1d1694f81ad49
mkielak <kielakowsky17@gmail.com>
2026-02-25 10:13:49 +0100

    Updated prisma in lib

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from "@/app/generated/prisma/client"
-import { withAccelerate } from "@prisma/extension-accelerate"
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
